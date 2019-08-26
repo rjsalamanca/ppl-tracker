3c3158b1dcef068f4aa66a78c50d5cc9ce1746da
RJ Salamanca <salamancaronito@gmail.com>
2019-08-26 17:00:59 -0400

    added sql statements

--- ppl-backend/sql/schema.sql
+++ ppl-backend/sql/schema.sql
@@ -1,1 +1,10 @@
-CREATE DATABASE ppl_tracker;
+-- CREATE DATABASE ppl_tracker;
+
+CREATE TABLE users
+(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(500),
+    last_name VARCHAR(500),
+    email VARCHAR(500),
+    password VARCHAR(500)
+);
---
+++ ppl-backend/sql/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO users
+    (first_name,last_name,email,password)
+VALUES('First Temp', 'Last Temp', 'test@email.com', '1');
