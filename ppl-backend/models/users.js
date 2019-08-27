723eeabec7c532f5b68a03d938486f0237ea0d99
RJ Salamanca <salamancaronito@gmail.com>
2019-08-27 17:43:43 -0400

    added conn.js to our ignore created a local conn file created a users model to connect to our database

---
+++ ppl-backend/models/users.js
@@ -0,0 +1,13 @@
+const db = require('./conn.js');
+
+class Users {
+    constructor(id, first_name, last_name, email, password) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.password = password;
+    }
+}
+
+module.exports = Users;
