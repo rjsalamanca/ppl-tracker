3d7a84f8359a434521683aa3fc9bc0766ceff124
RJ Salamanca <salamancaronito@gmail.com>
2019-11-17 22:53:25 -0500

    working on adding a modal when creating daily routine

--- ppl-client/src/components/system/addExercises.js
+++ ppl-client/src/components/system/addExercises.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
-import { Card, Button, Form, Alert } from "react-bootstrap";
+import { Card, Button, Form, Alert, Modal } from "react-bootstrap";
 
 class addExercises extends Component {
-    state = { test: 'testing state' }
+    state = {
+        test: 'testing state',
+        show: false,
+        setShow: false
+    }
+
+    Example() {
+        const { show, setShow } = this.state;
+
+        const handleClose = () => this.setState({ show: false });
+        const handleShow = () => this.setState({ show: true });
+
+        return (
+            <>
+                <Button variant="primary" onClick={handleShow}>
+                    Launch demo modal
+            </Button>
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                            Save Changes
+                </Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+        );
+    }
 
     render() {
         return (
             < div >
+                {this.Example()}
                 <p>Program</p>
                 <Form>
                     <Form.Group controlId="formBasicEmail">
