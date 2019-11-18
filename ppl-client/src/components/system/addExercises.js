67353abeb9882c3e0969ba148f2aff608fdfc515
RJ Salamanca <salamancaronito@gmail.com>
2019-11-18 02:40:16 -0500

    created an add day to your routine. Each day added will be placed on a modal and then be placed on the page once saved.

--- ppl-client/src/components/system/addExercises.js
+++ ppl-client/src/components/system/addExercises.js
@@ -5,51 +5,44 @@ class addExercises extends Component {
     state = {
         test: 'testing state',
         show: false,
-        setShow: false
+        days: []
     }
 
-    Example() {
-        const { show, setShow } = this.state;
-
-        const handleClose = () => this.setState({ show: false });
-        const handleShow = () => this.setState({ show: true });
-
-        return (
-            <>
-                <Button variant="primary" onClick={handleShow}>
-                    Launch demo modal
-            </Button>
-
-                <Modal show={show} onHide={handleClose}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Modal heading</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={handleClose}>
-                            Close
-                </Button>
-                        <Button variant="primary" onClick={handleClose}>
-                            Save Changes
-                </Button>
-                    </Modal.Footer>
-                </Modal>
-            </>
-        );
+    handleClose = () => this.setState({ show: false });
+    handleShow = () => this.setState({ show: true });
+    modalTrigger = () => {
+        const { show } = this.state;
+        !!show ? this.setState({ show: false }) : this.setState({ show: true })
     }
 
     render() {
+        const { show } = this.state;
         return (
             < div >
-                {this.Example()}
-                <p>Program</p>
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Routine Name</Form.Label>
-                        <Form.Control type="input" onChange={(e) => this.handleRoutine(e)} placeholder="Ex. Push Pull Legs" />
-                    </Form.Group>
+                <Modal show={show} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Add a Day to Your Routine</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
 
-                    <Button className="mb-3" variant="danger" onClick={(e) => this.createRoutine(e)}>Create</Button>
+                        Name of the Day:
+                <Button variant="secondary">
+                            Add Exercise
+                </Button>
+
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleClose}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+                <p> test Program</p>
+                <Form>
+                    <Button className="mb-3" variant="danger" onClick={(e) => this.modalTrigger(e)}>Add a Day</Button>
                 </Form>
             </div >
         );
