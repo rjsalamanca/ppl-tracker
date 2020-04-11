91fb427f2be13ab77da6a66f560b8202b362fc8f
RJ Salamanca <salamancaronito@gmail.com>
2020-04-11 14:14:31 -0400

    fixed login cookie issue still have a logout bug that requires us to logout twice

--- ppl-client/src/components/PrivateRoute.js
+++ ppl-client/src/components/PrivateRoute.js
@@ -8,35 +8,36 @@ function PrivateRoute({ ContextProvider, LoadComponent, ...routerProps }) {
    const [loadRoute, setLoadRoute] = useState();
    const [cookies] = useCookies(['user']);
    const { loggedIn, setLoggedIn } = useContext(UserContext);
+
    useEffect(() => {
       console.log('run first:', loggedIn)
       if (!!cookies.hasOwnProperty('user')) {
-         !!cookies.user.isLoggedIn ? setLoggedIn(true) : setLoggedIn(false);
+         if (!!loggedIn && !!cookies.user.isLoggedIn) {
+            setLoggedIn(true);
+         }
       } else {
-         console.log('in here')
          setLoggedIn(false);
       }
       // eslint-disable-next-line react-hooks/exhaustive-deps
    }, [cookies])
 
    useEffect(() => {
-      console.log('changed:', loggedIn)
-      if (!!loggedIn) {
-         checkLoginStatus();
+      if (!!cookies.hasOwnProperty('user')) {
+         if (!!loggedIn && !!cookies.user.isLoggedIn) {
+            checkLoginStatus();
+         } else if (!cookies.user.isLoggedIn) {
+            setLoadRoute(<Route render={() => <Redirect to='/' />} />)
+         }
       }
 
    }, [loggedIn])
 
    const checkLoginStatus = async () => {
-      if (!!loggedIn) {
-         setLoadRoute(
-            <Route {...routerProps} render={(props) =>
-               <ContextProvider LoadComponent={LoadComponent} RouterProps={props} />
-            } />
-         );
-      } else {
-         setLoadRoute(<Route render={() => <Redirect to='/' />} />)
-      }
+      setLoadRoute(
+         <Route {...routerProps} render={(props) =>
+            <ContextProvider LoadComponent={LoadComponent} RouterProps={props} />
+         } />
+      );
    }
 
    return <Switch>{loadRoute}</Switch>;
