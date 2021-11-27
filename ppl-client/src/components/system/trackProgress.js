de666c71d0f8d413e72aebe0f9e8d2c25f61a730
rjsalamanca <salamancaronito@gmail.com>
2021-11-27 02:38:26 -0500

    added new comnent - track progress

---
+++ ppl-client/src/components/system/trackProgress.js
@@ -0,0 +1,13 @@
+import React, { useState, useContext, useEffect } from 'react';
+
+
+function TrackProgress() {
+
+   return (
+      <div>
+         new component
+      </div>
+   );
+}
+
+export default TrackProgress;
